9403c0443be9d863699dfd679bd2447c7313075a
malitha-tishamal <malithatishamal@gmail.com>
2026-08-20 15:08:56 +0530

    Update index.tsx

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <section className='relative md:pt-44 pt-28 bg-white  dark:bg-darklight bg-cover text-white'>
+    <section className='relative md:pt-44 pt-28 bg-white dark:bg-darklight bg-cover text-white'>
       <div className='container mx-auto max-w-6xl px-4 grid grid-cols-12 gap-4 relative z-10'>
         <div
           className='md:col-span-6 col-span-12 p-4 md:px-4 px-0 space-y-4 flex flex-col items-start justify-center'
@@ -18,14 +18,14 @@ const Hero = () => {
               build everything
             </span>
           </div>
-          <h1 className='text-midnight_text font-bold dark:text-white text-4xl  md:text-5xl md:leading-[1.15]'>
+          <h1 className='text-midnight_text font-bold dark:text-white text-4xl md:text-5xl md:leading-[1.15]'>
             Unveiling My Professional Odyssey: Portfolio Highlights
           </h1>
           <p className='text-grey dark:text-white/70 text-xl font-semibold'>
             A brief introduction about myself and my professional objectives.
           </p>
           <a
-            href='#learn-more'
+            href='#contact'
             className='py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300 px-8'>
             Get Started
           </a>
@@ -59,23 +59,28 @@ const Hero = () => {
             <div>
               <p className='text-sm font-normal text-grey max-w-56'>
                 Need help?{' '}
-                <Link href='#' className='text-primary hover:text-blue-700'>
-                  Contact our experts
+                <Link href='#contact' className='text-primary hover:text-blue-700'>
+                  Contact me
                 </Link>{' '}
-                Tell us about your project
+                Tell me about your project
               </p>
             </div>
           </div>
         </div>
 
-        <div className="md:col-span-6 col-span-12 relative before:absolute before:content-[''] before:bg-[url('/images/hero/line-leyar.svg')] before:bg-no-repeat before:left-1/2 before:top-0 before:h-24 before:w-52 before:-z-10 before:translate-x-70% before:-translate-y-40% lg:before:inline-block before:hidden after:absolute after:content-[''] after:bg-[url('/images/hero/round-leyar.svg')] after:bg-no-repeat xl:after:inline-block after:hidden after:left-0 after:bottom-0 after:h-6.25 after:w-6.25 after:-z-10 after:-translate-x-1/2 after:translate-y-1/2">
+        <div
+          className="md:col-span-6 col-span-12 relative flex items-center justify-center before:absolute before:content-[''] before:bg-[url('/images/hero/line-leyar.svg')] before:bg-no-repeat before:left-1/2 before:top-0 before:h-24 before:w-52 before:-z-10 before:translate-x-70% before:-translate-y-40% lg:before:inline-block before:hidden after:absolute after:content-[''] after:bg-[url('/images/hero/round-leyar.svg')] after:bg-no-repeat xl:after:inline-block after:hidden after:left-0 after:bottom-0 after:h-6.25 after:w-6.25 after:-z-10 after:-translate-x-1/2 after:translate-y-1/2"
+          data-aos='fade-left'
+          data-aos-delay='200'
+          data-aos-duration='1000'>
           <Image
-            src={getImgPath('/images/hero/hero-image.png')}
-            alt='hero-image'
-            width={350}
-            height={150}
+            src={getImgPath('/images/hero/malitha-hero.jpg')}
+            alt='Malitha Tishamal'
+            width={450}
+            height={480}
             quality={100}
-            style={{ width: '100%', height: 'auto' }}
+            priority
+            className='rounded-3xl shadow-2xl object-cover max-h-[480px] w-auto border border-white/10'
           />
         </div>
       </div>
