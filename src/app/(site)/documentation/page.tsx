dd64958eb945c2a2cc57cfa58f9be8e8c41be2ef
malitha-tishamal <malithatishamal@gmail.com>
2026-08-20 15:09:30 +0530

    Update page.tsx

--- src/app/(site)/documentation/page.tsx
+++ src/app/(site)/documentation/page.tsx
@@ -2,7 +2,7 @@
 import { Documentation } from "@/components/Documentation/Documentation";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-    title: "Documentation | Venus",
+    title: "Documentation | Malitha Tishamal",
 };
 
 export default function Page() {
