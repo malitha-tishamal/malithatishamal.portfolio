378bab2822124dbc3ed45b0089b8b0fced607e86
malitha-tishamal <malithatishamal@gmail.com>
2026-08-20 15:09:03 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,10 @@ import Portfolio from '@/components/SharedComponent/portfollio'
 import Testimonial from '@/components/SharedComponent/Testimonial'
 import Blog from '@/components/SharedComponent/Blog'
 import Contactform from '@/components/Home/Contact';
+
 export const metadata: Metadata = {
-  title: "Venus",
+  title: "Malitha Tishamal – Full Stack Developer & DevOps Engineer | AI & Cybersecurity Specialist",
+  description: "Personal Portfolio & Professional Showcase of Malitha Tishamal",
 };
 
 export default function Home() {
