e2780d382051df60a7a0b35434c12854a8ba9583
malitha-tishamal <malithatishamal@gmail.com>
2026-08-21 12:32:40 +0530

    fix bugs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingExcludes: {
+    '*': ['./venus-nextjs-1.0.0/**', './venus-nextjs-1.0.0.zip'],
+  },
 };
 
 export default nextConfig;
