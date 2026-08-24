73b523b0052c73f6f868132c9bc4a6a2635256e8
malitha-tishamal <malithatishamal@gmail.com>
2026-08-24 18:06:59 +0530

    Update index.tsx

--- src/components/SharedComponent/HeroSub/index.tsx
+++ src/components/SharedComponent/HeroSub/index.tsx
@@ -11,11 +11,11 @@ interface HeroSubProps {
 const HeroSub: FC<HeroSubProps> = ({ title, description, breadcrumbLinks }) => {
   return (
     <>
-      <section className='text-center md:py-24 py-16 md:pt-44 pt-36 dark:bg-darkmode'>
-        <h2 className='dark:text-white md:text-[40px] leading-tight text-4xl font-bold text-midnight_text'>
+      <section className='text-center md:pt-36 pt-24 md:pb-4 pb-2 dark:bg-darkmode'>
+        <h2 className='dark:text-white md:text-[38px] leading-tight text-3xl font-bold text-midnight_text'>
           {title}
         </h2>
-        <p className='md:text-xl text-lg text-grey dark:text-white/50 font-normal max-w-45 w-full mx-auto my-[1.875rem] sm:px-0 px-4'>
+        <p className='md:text-lg text-base text-grey dark:text-white/50 font-normal max-w-45 w-full mx-auto my-3 sm:px-0 px-4'>
           {description}
         </p>
         <Breadcrumb links={breadcrumbLinks} />
