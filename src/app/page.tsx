7cb9f34b822a3195ffc2390834ca232dc4e94a68
malitha-tishamal <malithatishamal@gmail.com>
2026-08-26 00:23:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ import Counter from '@/components/Home/Counter'
 import Progresswork from '@/components/Home/WorkProgress';
 import Services from '@/components/Home/Services';
 import Portfolio from '@/components/SharedComponent/portfollio'
+import Projects from '@/components/SharedComponent/Projects'
 import Testimonial from '@/components/SharedComponent/Testimonial'
 import Blog from '@/components/SharedComponent/Blog'
 import Contactform from '@/components/Home/Contact';
+
 export const metadata: Metadata = {
   title: "Malitha Tishamal – Full Stack Developer & DevOps Engineer | AI & Cybersecurity Specialist",
 };
@@ -20,6 +22,7 @@ export default function Home() {
       <Progresswork isColorMode={false} />
       <Services />
       <Portfolio />
+      <Projects />
       <Testimonial />
       <Blog />
       <Contactform />
