61a400d87838dddfa568701a9c4eaee25326a3af
malitha-tishamal <malithatishamal@gmail.com>
2026-08-25 23:01:58 +0530

    Update index.tsx

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -50,84 +50,84 @@ const Hero = () => {
     setIsCvModalOpen(false)
   }
 
-  // Social Links List with Default Brand Colors
+  // Social Links List with Full Solid Brand Colors
   const socialLinks = [
     {
       id: 'linkedin',
       name: 'LinkedIn',
       url: heroData.linkedinUrl,
       icon: (
-        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24'>
           <path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z' />
         </svg>
       ),
-      baseClass: 'bg-[#0A66C2]/10 text-[#0A66C2] border-[#0A66C2]/30 hover:bg-[#0A66C2] hover:text-white hover:border-[#0A66C2] hover:shadow-lg hover:shadow-[#0A66C2]/25',
+      baseClass: 'bg-[#0A66C2] text-white shadow-md shadow-[#0A66C2]/30 hover:shadow-lg hover:shadow-[#0A66C2]/45 hover:-translate-y-1',
     },
     {
       id: 'github',
       name: 'GitHub',
       url: heroData.githubUrl,
       icon: (
-        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24'>
           <path fillRule='evenodd' clipRule='evenodd' d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.53 1.032 1.53 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z' />
         </svg>
       ),
-      baseClass: 'bg-gray-900/10 text-gray-900 dark:bg-white/10 dark:text-white border-gray-900/20 dark:border-white/20 hover:bg-gray-950 hover:text-white dark:hover:bg-white dark:hover:text-black hover:shadow-lg',
+      baseClass: 'bg-[#24292e] text-white dark:bg-white dark:text-[#24292e] shadow-md shadow-gray-900/20 dark:shadow-white/20 hover:shadow-lg hover:-translate-y-1',
     },
     {
       id: 'instagram',
       name: 'Instagram',
       url: heroData.instagramUrl,
       icon: (
-        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24'>
           <path d='M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z' />
         </svg>
       ),
-      baseClass: 'bg-pink-500/10 text-[#E1306C] border-pink-500/30 hover:bg-gradient-to-tr hover:from-[#f09433] hover:via-[#dc2743] hover:to-[#bc1888] hover:text-white hover:border-transparent hover:shadow-lg hover:shadow-pink-500/25',
+      baseClass: 'bg-gradient-to-tr from-[#f09433] via-[#dc2743] to-[#bc1888] text-white shadow-md shadow-pink-500/30 hover:shadow-lg hover:shadow-pink-500/45 hover:-translate-y-1',
     },
     {
       id: 'facebook',
       name: 'Facebook',
       url: heroData.facebookUrl,
       icon: (
-        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24'>
           <path d='M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z' />
         </svg>
       ),
-      baseClass: 'bg-[#1877F2]/10 text-[#1877F2] border-[#1877F2]/30 hover:bg-[#1877F2] hover:text-white hover:border-[#1877F2] hover:shadow-lg hover:shadow-[#1877F2]/25',
+      baseClass: 'bg-[#1877F2] text-white shadow-md shadow-[#1877F2]/30 hover:shadow-lg hover:shadow-[#1877F2]/45 hover:-translate-y-1',
     },
     {
       id: 'twitter',
       name: 'X (Twitter)',
       url: heroData.twitterUrl,
       icon: (
-        <svg className='w-4 h-4' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
           <path d='M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z' />
         </svg>
       ),
-      baseClass: 'bg-black/10 text-black dark:bg-white/10 dark:text-white border-black/20 dark:border-white/20 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black hover:shadow-lg',
+      baseClass: 'bg-black text-white dark:bg-white dark:text-black shadow-md shadow-black/20 dark:shadow-white/20 hover:shadow-lg hover:-translate-y-1',
     },
     {
       id: 'whatsapp',
       name: 'WhatsApp',
       url: heroData.whatsappUrl,
       icon: (
-        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24'>
           <path d='M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z' />
         </svg>
       ),
-      baseClass: 'bg-[#25D366]/10 text-[#25D366] border-[#25D366]/30 hover:bg-[#25D366] hover:text-white hover:border-[#25D366] hover:shadow-lg hover:shadow-[#25D366]/25',
+      baseClass: 'bg-[#25D366] text-white shadow-md shadow-[#25D366]/30 hover:shadow-lg hover:shadow-[#25D366]/45 hover:-translate-y-1',
     },
     {
       id: 'youtube',
       name: 'YouTube',
       url: heroData.youtubeUrl,
       icon: (
-        <svg className='w-4.5 h-4.5' fill='currentColor' viewBox='0 0 24 24'>
+        <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24'>
           <path d='M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z' />
         </svg>
       ),
-      baseClass: 'bg-[#FF0000]/10 text-[#FF0000] border-[#FF0000]/30 hover:bg-[#FF0000] hover:text-white hover:border-[#FF0000] hover:shadow-lg hover:shadow-[#FF0000]/25',
+      baseClass: 'bg-[#FF0000] text-white shadow-md shadow-[#FF0000]/30 hover:shadow-lg hover:shadow-[#FF0000]/45 hover:-translate-y-1',
     },
   ].filter((s) => s.url && s.url.trim().length > 0 && s.url !== '#')
 
