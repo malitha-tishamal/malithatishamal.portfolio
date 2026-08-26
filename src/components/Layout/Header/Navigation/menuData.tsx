558ee42d7b0e5e6a4316843fb0eaae004822bf9a
malitha-tishamal <malithatishamal@gmail.com>
2026-08-26 15:20:06 +0530

    Update menuData.tsx

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -4,6 +4,7 @@ export const headerData: HeaderItem[] = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
   { label: "Services", href: "/services" },
+  { label: "Certifications", href: "/certifications" },
   { label: "Portfolio", href: "/portfolio" },
   { label: "Projects", href: "/projects" },
   { label: "Testimonials", href: "/testimonials" },
