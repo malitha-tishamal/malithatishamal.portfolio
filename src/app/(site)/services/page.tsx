37a9720c1ec0fdf75a1951d8745ef3f9a64a70ac
malitha-tishamal <malithatishamal@gmail.com>
2026-09-16 21:23:42 +0530

    Update page.tsx

--- src/app/(site)/services/page.tsx
+++ src/app/(site)/services/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import HeroSub from "@/components/SharedComponent/HeroSub";
 import { Metadata } from "next";
 import Counter from "@/components/Home/Counter";
-import Progresswork from "@/components/Home/WorkProgress";
 import Services from "@/components/Home/Services";
 export const metadata: Metadata = {
     title: "Services | Venus",
